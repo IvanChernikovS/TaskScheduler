526e15f72e5caa102a2fe1b0a509c13c43c2ebd4
Ivan Chernikov <Ivan.Chernikov@tomtom.com>
2023-11-22 16:35:54 +0200

    formatting

--- TaskSchedulerImpl.h
+++ TaskSchedulerImpl.h
@@ -17,7 +17,7 @@
 class TaskSchedulerImpl : public ITaskScheduler {
 private:
     int taskIdCounter;
-    std::priority_queue<std::shared_ptr<Task>> taskQueue;
+    std::priority_queue <std::shared_ptr<Task>> taskQueue;
     std::unordered_set<int> taskIds;
     std::condition_variable cv;
     std::mutex mtx;
@@ -30,14 +30,15 @@ public:
     }
 
     int Schedule(std::function<void()> task, int delay, int priority, std::function<void()> callback) {
-        std::lock_guard<std::mutex> lock(mtx);
+        std::lock_guard <std::mutex> lock(mtx);
 
         int id = ++taskIdCounter;
         auto newTask = std::make_unique<Task>(id, task, callback, priority, delay);
         taskIds.emplace(newTask->taskId);
         taskQueue.push(std::move(newTask));
 
-        std::cout << "TaskScheduler::Schedule(): Task with id {" << id << "} and priority {" << priority << "} scheduled" << std::endl;
+        std::cout << "TaskScheduler::Schedule(): Task with id {" << id << "} and priority {" << priority
+                  << "} scheduled" << std::endl;
 
         cv.notify_all();
 
@@ -45,7 +46,7 @@ public:
     }
 
     void Cancel(int taskId) {
-        std::lock_guard<std::mutex> lock(mtx);
+        std::lock_guard <std::mutex> lock(mtx);
 
         if (taskIds.find(taskId) != taskIds.end()) {
             // TODO: cancel task in pool
@@ -57,14 +58,15 @@ public:
     }
 
     std::vector<int> GetIncompleteTaskIds() {
-        std::lock_guard<std::mutex> lock(mtx);
+        std::lock_guard <std::mutex> lock(mtx);
 
         std::vector<int> incompleteTaskIds;
-        for (const auto& id : taskIds) {
+        for (const auto &id: taskIds) {
             incompleteTaskIds.push_back(id);
         }
 
-        std::cout << "TaskScheduler::GetIncompleteTasks(): There are {" << incompleteTaskIds.size() << "} incomplete tasks" << std::endl;
+        std::cout << "TaskScheduler::GetIncompleteTasks(): There are {" << incompleteTaskIds.size()
+                  << "} incomplete tasks" << std::endl;
 
         return incompleteTaskIds;
     }
@@ -72,23 +74,25 @@ public:
     int GetEstimatedStartTime(int taskId) {
         // This function is placeholder since the estimation of start time is a complex problem
         // and depends on many factors such as task duration, priority, etc.
-        std::cout << "TaskScheduler::getEstimatedStartTime(): Estimating start time for task id {" << taskId << "}" << std::endl;
+        std::cout << "TaskScheduler::getEstimatedStartTime(): Estimating start time for task id {" << taskId << "}"
+                  << std::endl;
         return -1;
     }
 
     void Start() {
         isRunning = true;
         std::cout << "TaskScheduler::Start(): Starting TaskScheduler" << std::endl;
-        while(isRunning) {
+        while (isRunning) {
             std::this_thread::sleep_for(std::chrono::milliseconds(100));
-            std::lock_guard<std::mutex> lock(mtx);
-            while(!taskQueue.empty() && taskQueue.top()->startTime <= std::chrono::system_clock::now()){
+            std::lock_guard <std::mutex> lock(mtx);
+            while (!taskQueue.empty() && taskQueue.top()->startTime <= std::chrono::system_clock::now()) {
                 auto task = taskQueue.top();
                 pool.Enqueue(task);
                 taskQueue.pop();
                 taskIds.erase(task->taskId);
 
-                std::cout << "TaskScheduler::Start(): Task with id {" << task->taskId << "} and priority {" << task->priority << "} started" << std::endl;
+                std::cout << "TaskScheduler::Start(): Task with id {" << task->taskId << "} and priority {"
+                          << task->priority << "} started" << std::endl;
             }
         }
     }
